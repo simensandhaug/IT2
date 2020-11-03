d1f1bb45e740d9bd73f0d80225209fbdae07733a
simensandhaug <70633406+simensandhaug@users.noreply.github.com>
2020-11-03 16:07:15 +0100

    Create

--- Fahrenheit - Celsius/app.js
+++
@@ -1,8 +0,0 @@
-
-function calculate(){
-    let fahrenheit = document.getElementById("degrees").value;
-    let celsius = (fahrenheit - 32)*(5/9);
-    document.getElementById("celsius").innerHTML = celsius;
-}
-
-
