86a495aba5c9c58e0a8fb63b55a6b6f14f2ddaf4
simensandhaug <70633406+simensandhaug@users.noreply.github.com>
2020-11-30 12:13:23 +0100

    Oppgaver

--- JavaScript/PrøveOppgaver/js/oppgaver.js
+++ JavaScript/PrøveOppgaver/js/oppgaver.js
@@ -1,19 +1,36 @@
 //Oppgave 3
 
-const hovedsteder = ['Madrid', 'Oslo', 'Stockholm', 'København', 'Helsinki'];
-const land = ['Spania', 'Norge', 'Sverige', 'Danmark', 'Finnland']
-const dropdown = document.getElementById("dropdown");
-const outputEl = document.getElementById("output")
+// const hovedsteder = ['Madrid', 'Oslo', 'Stockholm', 'København', 'Helsinki'];
+// const land = ['Spania', 'Norge', 'Sverige', 'Danmark', 'Finnland']
+// const dropdown = document.getElementById("dropdown");
+// const outputEl = document.getElementById("output")
 
-for(l in land) {
-    dropdown.innerHTML+=` <option value="${land[l]}">${land[l]}</option>`
-}
+// for(l in land) {
+//     dropdown.innerHTML+=` <option value="${land[l]}">${land[l]}</option>`
+// }
 
-dropdown.addEventListener("change", () => {
-    outputEl.innerHTML =`"Du valgte ${dropdown.value}, som har hovedstad ${hovedsteder[land.indexOf(dropdown.value)]}"`;
-});
+// dropdown.addEventListener("change", () => {
+//     outputEl.innerHTML =`Du valgte ${dropdown.value}, som har hovedstad ${hovedsteder[land.indexOf(dropdown.value)]}`;
+// });
 
 //-------------------------------------------------------------------------------------------------------------------------------
 
-//Oppgave 4
+//Oppgave 5
 
+const output = document.getElementById("kuk");
+let tall = [];
+
+const generateNumbers = (num) => {
+    kuk.innerHTML = "";
+    for (let i = 0; i < num; i++) {
+        tall.push(Math.floor(Math.random() * 1) + 1);
+    }
+    tall.sort();
+    for (num in tall) {
+        kuk.innerHTML += `${tall[num]}, `;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//Oppgave 6
