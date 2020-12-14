1477392bf90a0aed0adafbb953c0ca4b6fc32ad4
simensandhaug <70633406+simensandhaug@users.noreply.github.com>
2020-12-14 10:27:35 +0100

    Canvas

--- JavaScript/Uviktig/Canvas/Face-Recognition/sketch.js
+++ JavaScript/Uviktig/Canvas/Face-Recognition/sketch.js
@@ -5,53 +5,59 @@ let skeleton;
 let ctx;
 let canvas;
 
-function setup(){
+let colorPositions = document.querySelectorAll('input[type="radio"]');
+console.log(colorPositions);
+for(el in colorPositions) {
+    console.log(getOffset(colorPositions[el]));
+}
+
+
+function setup() {
     createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
     background(240);
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose',gotPoses);
+    poseNet.on('pose', gotPoses);
 
-		canvas = document.querySelector('#drawCanvas');
-		ctx = canvas.getContext('2d');
+    canvas = document.querySelector('#drawCanvas');
+    ctx = canvas.getContext('2d');
 }
 
-function gotPoses(poses){
-    console.log(poses);
-    if(poses.length>0){
+function gotPoses(poses) {
+    if (poses.length > 0) {
         pose = poses[0].pose;
         skeleton = poses[0].skeleton;
     }
 
-		if (pose) {
+    if (pose) {
 
-			ctx.fillRect(canvas.width - pose.nose.x, pose.nose.y, 10, 10);
+        ctx.fillRect(canvas.width - pose.rightWrist.x, pose.rightWrist.y, 10, 10);
 
-		}
+    }
 
 
 
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log('ready');
 }
 
 function draw() {
-translate(width,0); // move to far corner
-	scale(-1.0,1.0);    // flip x-axis backwards
-    image(video,0,0,width,height);
+    translate(width, 0); // move to far corner
+    scale(-1.0, 1.0); // flip x-axis backwards
+    image(video, 0, 0, width, height);
 
-		if(pose) {
+    if (pose) {
 
-      let righteye=pose.rightEye;
-      let lefteye=pose.leftEye;
-      let distance = dist(righteye.x,righteye.y,lefteye.x,lefteye.y);
+        let righteye = pose.rightEye;
+        let lefteye = pose.leftEye;
+        let distance = dist(righteye.x, righteye.y, lefteye.x, lefteye.y);
 
-	    fill(255,0,0);
-	    ellipse(pose.nose.x, pose.nose.y,distance/2);
+        fill(255, 0, 0);
+        ellipse(pose.rightWrist.x, pose.rightWrist.y, distance / 2);
 
-		}
+    }
 
 }
