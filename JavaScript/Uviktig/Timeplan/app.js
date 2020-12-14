b1e497899e4e848074e8ed2df2ac3cfd491e37a4
= <=>
2020-12-14 14:12:52 +0100

    Fiksa digge saker

--- JavaScript/Uviktig/Timeplan/app.js
+++ JavaScript/Uviktig/Timeplan/app.js
@@ -1,32 +1,42 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const days = document.querySelectorAll(".day");
 
-let displayDate = document.getElementById("displayDate");
-let displayTime = document.getElementById("displayTime");
 let displayFulldate = document.getElementById("fulldate");
 let date = new Date();
 
+let xStart = 0;
+let xEnd = canvas.width;
+
+console.log(days[0])
+
 const cNum = n => (n).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
 
-displayDate.innerHTML = `${cNum(date.getDate())}.${cNum(date.getMonth())}.${date.getFullYear()}`;
-displayTime.innerHTML = `${cNum(date.getHours())}.${cNum(date.getMinutes())}`;
 displayFulldate.innerHTML = `${date}`;
 
 setInterval(() => {
     date = new Date();
 
+    for(day in days) {
+        if(day+1 == date.getDay()) {
+            const el = days[day].getBoundingClientRect();
+            xStart = el.x - 8 ;
+            xEnd = el.x - 8 + el.width;
+        }
+    }
+
     displayFulldate.innerHTML = `${date}`;
-    displayDate.innerHTML = `${cNum(date.getDate())}.${cNum(date.getMonth())}.${date.getFullYear()}<br>`;
-    displayTime.innerHTML = `${cNum(date.getHours())}.${cNum(date.getMinutes())}`;
     const y = canvas.height/451 * ((date.getHours()-8)*60 + date.getMinutes());
-    console.log(canvas.height)
+
+
+
     if(date.getHours() >= 8 && date.getHours() < 16) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.beginPath()
             ctx.lineWidth = 2;
             ctx.strokeStyle = 'red';
-            ctx.moveTo(0, y);
-            ctx.lineTo(canvas.width, y);
+            ctx.moveTo(xStart, y);
+            ctx.lineTo(xEnd, y);
             ctx.stroke();
     }
 }, 1000);
