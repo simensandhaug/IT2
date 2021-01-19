7b3443e2381bc6a5ed83b44773c4a58a0cfb84ab
simensandhaug <simen.sandhaug@gmail.com>
2021-01-19 22:51:03 +0100

    MegaUpdate

--- JavaScript/Uviktig/18.Jan/Quiz 2/app.js
+++ JavaScript/Uviktig/18.Jan/Quiz 2/app.js
@@ -8,6 +8,7 @@ let containerEl = document.getElementById("container");
 let nxtButtonEl = document.getElementById("nxtButton");
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let endResultEl = document.getElementById("endResults");
 let currentQ = 0;
 let score = 0;
 let question;
@@ -15,45 +16,244 @@ let question;
 nxtButtonEl.innerHTML = `<button id="next">Next</button>`;
 
 const questions = [{
-        q: "Hva er Elon Musk?",
+        q: "What is the capital of Chile?",
         choices: {
-            a: "Entrepenør",
-            b: "Fashiondesigner",
-            c: "Bilmekaniker"
+            a: "Sinaloa",
+            b: "Santiago",
+            c: "Brazilia",
+            d: "Tongo",
         },
         type: "Single Choice",
-        ans: "a"
+        ans: "b",
+        playerAnswer: "",
     },
     {
-        q: "Hva heter Asias største øy?",
+        q: "What is the highest mountain in Britain?",
         choices: {
-            a: "Korsika",
-            b: "Borneo",
-            c: "Bali"
+            a: "Mt. Chiliad",
+            b: "Ben Nevis",
+            c: "Mt. Coronet",
+            d: "Snowdon",
         },
         type: "Single Choice",
-        ans: "b"
+        ans: "b",
+        playerAnswer: "",
     },
     {
-        q: "I hvilket hav befinner Marianegropen seg?",
+        q: "What is the smallest country in the world?",
         choices: {
-            a: "Middelhavet",
-            b: "Atlanterhavet",
-            c: "Stillehavet"
+            a: "Vatican City",
+            b: "Montenegro",
+            c: "San Marino",
+            d: "Monaco"
         },
         type: "Single Choice",
-        ans: "c"
+        ans: "a",
+        playerAnswer: "",
     },
     {
-        q: "Hvilket grunnstoffer er med i vann?",
+        q: "Which of these countries have never been in a world war?",
         choices: {
-            a: "Oksygen",
-            b: "Helium",
-            c: "Hydrogen",
-            d: "Nitrogen"
+            a: "Switzerland",
+            b: "Norway",
+            c: "Sweden",
+            d: "Belgium",
         },
         type: "Multiple Choice",
-        ans: "a,c"
+        ans: "a,c",
+        playerAnswer: "",
+    },
+    {
+        q: "Alberta is a province i which country?",
+        choices: {
+            a: "South-Africa",
+            b: "Britain",
+            c: "USA",
+            d: "Canada",
+        },
+        type: "Single Choice",
+        ans: "d",
+        playerAnswer: "",
+    },
+    {
+        q: "How many time zones are there in Russia?",
+        choices: {
+            a: "12",
+            b: "13",
+            c: "11",
+            d: "8",
+        },
+        type: "Single Choice",
+        ans: "c",
+        playerAnswer: "",
+    },
+    {
+        q: "How many stripes are there on the US flag?",
+        choices: {
+            a: "13",
+            b: "11",
+            c: "17",
+            d: "52",
+        },
+        type: "Single Choice",
+        ans: "a",
+        playerAnswer: "",
+    },
+    {
+        q: "Which of the following empires had no written language?",
+        choices: {
+            a: "Aztec",
+            b: "Roman",
+            c: "Incan",
+            d: "Egyptian",
+        },
+        type: "Single Choice",
+        ans: "c",
+        playerAnswer: "",
+    },
+    {
+        q: "What country has the most islands in the world?",
+        choices: {
+            a: "South-Africa",
+            b: "Italy",
+            c: "Norway",
+            d: "Sweden",
+        },
+        type: "Single Choice",
+        ans: "d",
+        playerAnswer: "",
+    },
+    {
+        q: "In what ocean is the lowest natural place on planet Earth?",
+        choices: {
+            a: "Mediterranean",
+            b: "Atlantic Ocean",
+            c: "Pacific Ocean",
+            d: "Arctic Ocean",
+        },
+        type: "Single Choice",
+        ans: "c",
+        playerAnswer: "",
+    },
+    {
+        q: "What city do The Beatles come from?",
+        choices: {
+            a: "Manchester",
+            b: "Liverpool",
+            c: "Birmingham",
+            d: "London",
+        },
+        type: "Single Choice",
+        ans: "b",
+        playerAnswer: "",
+    },
+    {
+        q: "When was Netflix founded?",
+        choices: {
+            a: "2015",
+            b: "2009",
+            c: "2001",
+            d: "1997",
+        },
+        type: "Single Choice",
+        ans: "d",
+        playerAnswer: "",
+    },
+    {
+        q: "What is Disney's first film?",
+        choices: {
+            a: "Mickey Mouse's Adventures",
+            b: "Lady and the Tramp",
+            c: "Snow White",
+            d: "Frozen",
+        },
+        type: "Single Choice",
+        ans: "c",
+        playerAnswer: "",
+    },
+    {
+        q: "What was the most-watched series on Netflix in 2019?",
+        choices: {
+            a: "Stranger Things",
+            b: "Game Of Thrones",
+            c: "Breaking Bad",
+            d: "The Queen's Gambit",
+        },
+        type: "Single Choice",
+        ans: "a",
+        playerAnswer: "",
+    },
+    {
+        q: "In which year was Joan of Arc burned at the stake?",
+        choices: {
+            a: "800 BC",
+            b: "1830 AD",
+            c: "1945 AD",
+            d: "1431 AD",
+        },
+        type: "Single Choice",
+        ans: "d",
+        playerAnswer: "",
+    },
+    {
+        q: "Which team has won the Champions League most times?",
+        choices: {
+            a: "Manchester United",
+            b: "Barcelona",
+            c: "Real Madrid",
+            d: "Juventus",
+        },
+        type: "Single Choice",
+        ans: "c",
+        playerAnswer: "",
+    },
+    {
+        q: "What is the most famous Mexican beer?",
+        choices: {
+            a: "Tecate",
+            b: "Bud Light",
+            c: "Budweiser",
+            d: "Corona",
+        },
+        type: "Single Choice",
+        ans: "d",
+        playerAnswer: "",
+    },
+    {
+        q: "What is the capital of Westeros in Game of Thrones?",
+        choices: {
+            a: "Dragonstone",
+            b: "King's Landing",
+            c: "Winterfell",
+            d: "Highgarden",
+        },
+        type: "Single Choice",
+        ans: "b",
+        playerAnswer: "",
+    },
+    {
+        q: "Which of these ARE Bob Dylan songs?",
+        choices: {
+            a: "Like A Rolling Stone",
+            b: "Back In Black",
+            c: "The Times They Are a-Changin'",
+            d: "Hotel California",
+        },
+        type: "Multiple Choice",
+        ans: "a,c",
+        playerAnswer: "",
+    },
+    {
+        q: "Which singer was known amongst other things as 'The King of Pop'?",
+        choices: {
+            a: "Elvis Presley",
+            b: "Michael Jackson",
+            c: "Freddie Mercury",
+            d: "Nick Jonas",
+        },
+        type: "Single Choice",
+        ans: "b",
+        playerAnswer: "",
     },
 ];
 
@@ -72,12 +272,12 @@ class Question {
         const radioOutput = [];
         if (this.type == "Single Choice")
             for (const [key, value] of Object.entries(this.choices)) {
-                labelOutput.push(`<label for="radio">${value}</label>`);
+                labelOutput.push(`<label for="radio">${key}. ${value}</label>`);
                 radioOutput.push(`<input type="radio" name="radio" id="${key}" class="choice">`);
             }
         else
             for (const [key, value] of Object.entries(this.choices)) {
-                labelOutput.push(`<label for="radio">${value}</label>`);
+                labelOutput.push(`<label for="radio">${key}. ${value}</label>`);
                 radioOutput.push(`<input type="checkbox" id="${key}" class="choice">`);
             }
         radioContainerEl.innerHTML = radioOutput.join("<br>");
@@ -124,6 +324,15 @@ const endQuiz = () => {
     nxtButtonEl.innerHTML = "";
 
     drawResult(score, currentQ);
+
+    questions.forEach(question => {
+        endResultEl.innerHTML += `<h2>${question.q}</h2>`
+        endResultEl.innerHTML += `<div class="endScreenQuestions">`;
+        for (const [key, value] of Object.entries(question.choices)) endResultEl.innerHTML += `${key}. ${value}<br>`;
+        endResultEl.innerHTML += question.ans == question.playerAnswer ? `<p style="color:green">Your Answer: ${question.playerAnswer}</p>`: `<p style="color:red">Your Answer: ${question.playerAnswer}</p>`;
+        endResultEl.innerHTML += `<p style="color:green">Correct Answer: ${question.ans}</p>`;
+        endResultEl.innerHTML += `</div>`
+    });
 }
 
 const nextQ = qNr => {
@@ -134,6 +343,7 @@ const nextQ = qNr => {
 }
 
 document.getElementById("next").addEventListener("click", () => {
+    const playerAnswer = [];
     const choices = Array.from(document.querySelectorAll(".choice"));
     if (question.getType == "Single Choice") choices.forEach(choice => {
         if (choice.checked && choice.id == question.getAns) score++
@@ -143,12 +353,18 @@ document.getElementById("next").addEventListener("click", () => {
         let correctAns = true;
         choices.forEach(choice => {
             answers.forEach(ans => {
-                if (choice.checked && choice.id == ans && correctAns) answers.splice(answers.indexOf(ans), 1);
-                else if (choice.checked && choice.id != ans) correctAns = false;
+                if (choice.checked && ans.includes(choice.id) && correctAns) answers.splice(answers.indexOf(ans), 1);
+                else if (choice.checked && !ans.includes(choice.id)) correctAns = false;
             });
         });
         if (answers.length == 0 && correctAns) score++;
     }
+    choices.forEach(choice => {
+        if(choice.checked) playerAnswer.push(choice.id);
+    });
+    if(question.getType == "Multiple Choice") questions[currentQ].playerAnswer = playerAnswer.join(",");
+    else questions[currentQ].playerAnswer = playerAnswer.join("");
+    console.log(questions[currentQ].playerAnswer)
     choices.forEach(choice => choice.checked = false);
     currentQ++;
     nextQ(currentQ);
